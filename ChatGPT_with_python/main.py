dd1fb2e03408e0dc1eb84399a3ea47f211a4114d
huzaif Ur Rehman Aamir <83422321+hudhayfah36@users.noreply.github.com>
2023-06-16 23:19:34 +0500

    Update main.py

--- ChatGPT_with_python/main.py
+++ ChatGPT_with_python/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import openai
+API_KEY = "xxxx-xxxx-xxxx" # enter your API key here
+openai.api_key = API_KEY
+
+response = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    messages=[
+            {"role": "user", "content": "who are you?"}
+        ]
+)
+print(response)
