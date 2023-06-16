072ff97b7fc91d6802a089700ae14a053757a205
huzaif Ur Rehman Aamir <83422321+hudhayfah36@users.noreply.github.com>
2023-06-16 23:30:27 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import openai
+API_KEY = "xxxx-xxxx-xxxx" # enter your API key here
+openai.api_key = API_KEY
+
+response = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    messages=[
+            {"role": "user", "content": "who are you?"}
+        ]
+)
+#print(response)
+print(response["choices"][0]["message"]["content"])
